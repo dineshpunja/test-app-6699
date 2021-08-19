c677d1e4d19700d22df9f8fa7b8f67bb19441df7
dineshpunja <dineshpunja4@gmail.com>
2021-08-19 15:23:09 +0530

    Changes in config file

--- config/production.js
+++ config/production.js
@@ -1,6 +1,5 @@
 module.exports = {
-
-    // changed db name from: getircase-study to getir-case-study
-    mongoURI: 'mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getircase-study?retryWrites=true' 
-
-}
+  // changed db name from: getircase-study to getir-case-study
+  mongoURI:
+    "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true",
+};
